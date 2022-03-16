cd6f0cfe3a0b8661e47860f7f8cf73f5582f72e7
Jacob Alonzo <alonzojpcpp@gmail.com>
2022-03-15 22:35:26 -0700

    First Commit

---
+++ src/CalculatorTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
