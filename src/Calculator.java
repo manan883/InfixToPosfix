e1549ca62700defb0a5716ae1480ccec5191cb2a
Jacob Alonzo <alonzojpcpp@gmail.com>
2022-03-16 21:49:43 -0700

    added all operands, program is currently working

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,16 +2,7 @@
 public class Calculator {
 
 	public static void main(String[] args) {
-//	    System.out.println("testing start");
-//	    StackInterface<Integer> stack1 = new ResizableArrayStack<>();
-//		stack1.push(1);
-//		stack1.push(2);
-//		stack1.push(3);
-//		stack1.pop();
-//		System.out.println(stack1.peek());
-//		System.out.println("testing complete");
-		
-		System.out.println(evaluatePostfix("61/2/"));
+		System.out.println(evaluatePostfix("23*42-/56*+"));
 	}
 
 
@@ -24,8 +15,6 @@ public class Calculator {
 	    int nextCharacterInt;
 	    int leftValue;
 	    int rightValue;
-	    int leftValueInt;
-	    int rightValueInt;
 	    int resultValue;
 	    
 	    while (tempCount < postfixLength) {
@@ -33,23 +22,47 @@ public class Calculator {
 	    		    	
 	    	switch(nextCharacter) {
 	    		case '0' : case '1' : case '2' : case '3' : case '4' : case '5' : case '6' : case '7' : case '8' : case '9' :
-	    			
 	    			nextCharacterInt = Character.getNumericValue(nextCharacter);
     				valueStack.push(nextCharacterInt);
 	    			System.out.println(nextCharacterInt + " has been pushed");
-
     			break;
     			
     			
 	    		case '+' : case '-' : case '*' : case '/' : case '^' :
+    				if (nextCharacter == '+') {
+    	    			rightValue = valueStack.pop();
+    	    			leftValue = valueStack.pop();
+    	    			resultValue = leftValue+rightValue;
+    	    			valueStack.push(resultValue);
+    					System.out.println(resultValue + " is the result of " + leftValue + " + " + rightValue);
+    				}
+    				if (nextCharacter == '-') {
+    	    			rightValue = valueStack.pop();
+    	    			leftValue = valueStack.pop();
+    	    			resultValue = leftValue-rightValue;
+    	    			valueStack.push(resultValue);
+    					System.out.println(resultValue + " is the result of " + leftValue + " - " + rightValue);
+    				}
+    				if (nextCharacter == '*') {
+    	    			rightValue = valueStack.pop();
+    	    			leftValue = valueStack.pop();
+    	    			resultValue = leftValue*rightValue;
+    	    			valueStack.push(resultValue);
+    					System.out.println(resultValue + " is the result of " + leftValue + " * " + rightValue);
+    				}
     				if (nextCharacter == '/') {
-
     	    			rightValue = valueStack.pop();
     	    			leftValue = valueStack.pop();
     	    			resultValue = leftValue/rightValue;
     	    			valueStack.push(resultValue);
-    					System.out.println(resultValue + " is the result");
-
+    					System.out.println(resultValue + " is the result of " + leftValue + " / " + rightValue);
+    				}
+    				if (nextCharacter == '^') {
+    	    			rightValue = valueStack.pop();
+    	    			leftValue = valueStack.pop();
+    	    			resultValue = leftValue^rightValue;
+    	    			valueStack.push(resultValue);
+    					System.out.println(resultValue + " is the result of " + leftValue + " ^ " + rightValue);
     				}
 	    		break;
 	    		
