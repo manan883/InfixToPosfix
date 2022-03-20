eea032ef4ac423a5b21f945bb1a4a7417b842704
manan Patel <manan883@gmail.com>
2022-03-19 23:53:08 -0700

    updated for node

--- src/LinkedBagStack.java
+++ src/LinkedBagStack.java
@@ -1,38 +1,67 @@
 import java.util.*;
 public class LinkedBagStack<T> implements StackInterface<T> {
+private Node topNode;	
 Vector<T> stack = new Vector<T>();
-//if (isEmpty())
-//throw new EmptyStackException();
+private class Node {
+        Object data;
+        Node next;
+        Node(Object d, Node topNode)
+        {
+            this.data = d;
+            next = topNode;
+        }
+        void setData(T newData) {
+        	data = newData;
+        }
+        void setNextNode(Node nextNode) {
+        	next = nextNode;
+        }
+        
+		public Node getNextNode() {
+			// TODO Auto-generated method stub
+			return next;
+		}
+		public Object getData() {
+			// TODO Auto-generated method stub
+			return data;
+		}
+}
+
+
+
+
+
 	@SuppressWarnings("unchecked")
 	public void push(Object newEntry) {
 		// TODO Auto-generated method stub
-		stack.add((T) newEntry);
+		Node newNode = new Node(newEntry, topNode);
+	      topNode = newNode;
 	}
 
 	public T pop() {
-		// TODO Auto-generated method stub
-		  if (isEmpty())
-		      throw new EmptyStackException();
-		   else
-		      return stack.remove(stack.size() - 1);
+		   T top = peek();  // Might throw EmptyStackException
+		   // Assertion: topNode != null
+		   topNode = topNode.getNextNode();
+		   return top;
 	}
 
+	@SuppressWarnings("unchecked")
 	public T peek() {
 		// TODO Auto-generated method stub
 		   if (isEmpty())
 			      throw new EmptyStackException();
 			   else
-			      return stack.lastElement();
+			      return (T) topNode.getData();
 	}
 
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		  return stack.isEmpty();
+		   return topNode == null;
 	}
 
 	public void clear() {
 		// TODO Auto-generated method stub
-		   stack.clear();
+		  topNode = null;
 		
 	}
 
