e3be788af828af6ba77879ccfdd9112aa79a7795
Jacob Alonzo <alonzojpcpp@gmail.com>
2022-03-20 23:58:33 -0700

    test2

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 		System.out.println(evaluatePostfix("23*42-/56*+"));
 		
 	}
-	// test
+	// test test
 	/** Evaluates a postfix expression using a ResizableArrayStack object.
     @param postfixString  The string that contains the postfix expression.
     @return Returns the top value in the stack which is the final result of the expression.
